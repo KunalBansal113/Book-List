c0f16ed6b73cbcc80e5e2ab84bdf2558cb35b0b0
KunalBansal113 <Kunalbansal113@gmail.com>
2025-09-19 14:29:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,11 @@ mongoose.connect(process.env.MONGO_URI)
 
 // API routes
 app.use('/api/books', bookRoutes);
+// in server.js (after app.use('/api/books', bookRoutes))
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
